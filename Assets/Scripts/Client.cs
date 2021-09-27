8ad17490688f6a423d296ef60211cbb9aec06aeb
Elias <elias.wasielewski@elev.ga.ntig.se>
2021-09-27 22:17:53 +0200

    Network uwu work

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using LiteNetLib;
+using LiteNetLib.Utils;
 using UnityEngine;
 
 public class Client : MonoBehaviour
 {
-    // Start is called before the first frame update
+    EventBasedNetListener netListener;
+    NetManager client;
+    NetPacketProcessor netPacketProcessor;
+
     void Start()
     {
-        
+        netListener = new EventBasedNetListener();
+        netPacketProcessor = new NetPacketProcessor();
+
+        netListener.PeerConnectedEvent += (server) =>
+        {
+            Debug.LogError($"Connected to server: {server.EndPoint}");
+        };
+
+        netListener.NetworkReceiveEvent += (server, reader, deliveryMethod) =>
+        {
+            netPacketProcessor.ReadAllPackets(reader, server);
+        };
+
+        netPacketProcessor.SubscribeReusable<FooPacket>((packet) =>
+        {
+            Debug.Log("Got a foo packet!");
+            Debug.Log(packet.NumberValue);
+        });
+
+        client = new NetManager(netListener);
+        client.Start();
+        client.Connect("192.168.0.69", 9050, "uwu");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        client.PollEvents();
     }
+}
+public class FooPacket
+{
+    public int NumberValue { get; set; }
+    public string StringValue { get; set; }
 }
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using LiteNetLib;
+using LiteNetLib.Utils;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    EventBasedNetListener netListener;
+    NetManager server;
+    NetPacketProcessor netProcessor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        netListener = new EventBasedNetListener();
+        server = new NetManager(netListener);
+        netProcessor = new NetPacketProcessor();
+
+        server.Start(9050);
+
+        netListener.ConnectionRequestEvent += request =>
+        {
+            if (server.ConnectedPeersCount < 10 /* max connections */)
+                request.AcceptIfKey("uwu");
+            else
+                request.Reject();
+        };
+
+        netListener.PeerConnectedEvent += (client) =>
+        {
+            Debug.LogError($"Client connected: {client.EndPoint}");
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("h"))
+        {
+            foreach (NetPeer peer in server.ConnectedPeerList)
+            {
+                netProcessor.Send(peer, new FooPacket() { NumberValue = 1, StringValue = "Test" }, DeliveryMethod.ReliableOrdered);
+            }
+        }
+        server.PollEvents();
+    }
+}
